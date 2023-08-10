306fa98b878355fb2d3be1ec1fe1d47ea9f88385
Adrian Sun <58319029+syfyufei@users.noreply.github.com>
2023-08-10 16:35:20 +0800

    setup

---
+++ knit.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+cd ./data/quarto_file
+quarto render
+
+cd ../../
+
+awk '/\\bookmarksetup{startatroot}/ {flag=1; next} /\\hypertarget{sec-appendix}/ {flag=0} flag' data/quarto_file/_book/book-latex/quarto_file.tex > data/main.tex
+
+awk '/\\hypertarget{sec-appendix}/ {flag=1} /\\end{document}/ {flag=0} flag' data/quarto_file/_book/book-latex/quarto_file.tex > data/appendix.tex
+
+make clean
+make thesis
